2f2f39e12ddce82e034a83f1135e591e3aa98262
kahneraja <kahneraja@gmail.com>
2018-02-11 12:50:22 -0500

    clean up gateway unit tests

--- app/controllers/github/tokens_controller.rb
+++ app/controllers/github/tokens_controller.rb
@@ -7,6 +7,7 @@ class Github::TokensController < ApplicationController
   def create
     token = @githubGateway.createToken(params[:client_id],params[:client_secret],params[:code])
     currentUser = @githubGateway.getCurrentUser(token['access_token'])
+
     render json: currentUser
   end
 
--- out/test/PullRequestAssistantAPI/gateways/github_gatway_test.rb
+++ out/test/PullRequestAssistantAPI/gateways/github_gatway_test.rb
@@ -8,7 +8,7 @@ class GithubGatewayTest < ActiveSupport::TestCase
                                     '{"access_token":"ABC","token_type":"bearer","scope":"read"}')
     gateway = GithubGateway.new(httpClient)
     token = gateway.createToken('', '', '')
-    assert_equal token['access_token'], 'ABC'
+    assert token['access_token'] == 'ABC'
   end
 
   test 'getCurrentUser should return a user when successful' do
@@ -16,7 +16,7 @@ class GithubGatewayTest < ActiveSupport::TestCase
                                     '{"id": 123}')
     gateway = GithubGateway.new(httpClient)
     currentUser = gateway.getCurrentUser('')
-    assert_equal currentUser['id'], 123
+    assert currentUser['id'] == 123
   end
 
 end
